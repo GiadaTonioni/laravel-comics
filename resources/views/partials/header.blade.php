a3e827319862b3a54fb03980ab8b5dd4c7de41bb
GiadaTonioni <giada.tonioni@gmail.com>
2023-07-27 16:22:24 +0200

    impostazione partial header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,16 @@
+<header>
+    <div class="header-top">
+
+    </div>
+    <div class="header-bottom">
+        <div class="container">
+            <div class="navbar">
+                <div class="logo">
+                    <a href="{{ url('/') }}">
+                        <img src="{{ Vite::asset('/resources/img/dc-logo.png') }}">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
